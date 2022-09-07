8223ad1bb809833cf25e60fbf2fbefea755f0fe4
miguelcs18 <miguelcs18@gmail.com>
2022-09-07 11:47:59 -0500

    Reverse array logic

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,17 @@ public class Main
 {
     public static void main(String[] args) {
 
+
     }
 
     public static void reverse(int[] array)
     {
+        int temp;
         for(int i=0; i<array.length; i++)
         {
-
+            temp = array[i];
+            array[i] = array[array.length-1];
+            array[array.length-1]=temp;
         }
     }
 }
