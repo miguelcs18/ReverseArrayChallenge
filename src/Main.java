a54fd44c13299b8953805279d57a1785c5506142
miguelcs18 <miguelcs18@gmail.com>
2022-09-07 11:49:51 -0500

    Print normal array

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 public class Main
 {
     public static void main(String[] args) {
         int [] array = {1,2,3,4,5};
-
+        System.out.println("array = " + Arrays.toString(array));
     }
 
     public static void reverse(int[] array)
