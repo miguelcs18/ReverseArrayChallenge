61a874ef7e3ed2cc96caf61fca10f18578e91d10
miguelcs18 <miguelcs18@gmail.com>
2022-09-08 13:03:24 -0500

    Change logic to reverse Array

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main
 {
     public static void main(String[] args) {
-        int [] array = {1,2,3,4,5};
+        int [] array = {11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
         System.out.println("array = " + Arrays.toString(array));
         reverse(array);
         System.out.println("reverse array = " + Arrays.toString(array));
@@ -11,12 +11,14 @@ public class Main
 
     public static void reverse(int[] array)
     {
-        int temp;
-        for(int i=0; i<array.length; i++)
+        int first;
+        int last;
+        for(int i=0; i<array.length/2; i++)
         {
-            temp = array[i];
-            array[i] = array[array.length-1];
-            array[array.length-1]=temp;
+         first = array[i];
+         last = array[array.length-(i+1)];
+         array[array.length-(i+1)] = first;
+         array[i] = last;
         }
     }
 }
